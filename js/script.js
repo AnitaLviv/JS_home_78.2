1d189c92a5baa5e79ed27f941eb20dbfbbbbefc4
AnitaLviv <annet_v@ukr.net>
2016-11-19 09:50:37 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,51 +1,21 @@
-$(function() {
+$(function(){
+    var $helpText = $('.helpText');
+    var $btn = $('#btn');
+    
+    $btn.on('click', function(){
+        $helpText.fadeIn(500);   
+    });
+        $('.named').hover(function(e){
+            var $input = $(e.target);
+            $input.siblings($helpText).fadeIn(500);
+        },function(e){
+            var $input = $(e.target);
+            $input.siblings($helpText).fadeOut(500);
+        
+        });
+    
+    
+    
   
- $( "#firstname" ).after( "<span class='firstname'>Please enter your firstname.</span>" );
- $(".firstname").css("opacity", "0");
-    
- $('#firstname').hover(
-     function(){
-       $(".firstname").animate({
-        opacity: 0.7,
-        }, 1500 );
-     },
-     function(){
-       $(".firstname").animate({
-        opacity: 0,
-        }, 1000 );
- });
- 
-  
-  $( "#lastname" ).after( "<span class='lastname'>Please enter your lastname.</span>" );
- $(".lastname").css("opacity", "0");
-    
- $('#lastname').hover(
-     function(){
-       $(".lastname").animate({
-        opacity: 0.7,
-        }, 1500 );
-     },
-     function(){
-       $(".lastname").animate({
-        opacity: 0,
-        }, 1000 );
- });
- 
- 
- $( "#email" ).after( "<span class='email'>Put your email.</span>" );
- $(".email").css("opacity", "0");
-    
- $('#email').hover(
-     function(){
-       $(".email").animate({
-        opacity: 0.7,
-        }, 1500 );
-     },
-     function(){
-       $(".email").animate({
-        opacity: 0,
-        }, 1000 );
- });
-       
-
-}); 
+                 
+});
