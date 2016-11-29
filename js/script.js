853208ba28d2415818e49e0871697ec687492c0b
AnitaLviv <annet_v@ukr.net>
2016-11-29 20:32:25 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,22 +1,75 @@
 
 
+(function() {
+  var $wrapper = $('<div>', {class: 'wrapper'}).appendTo('body');
+  var $form = $('<form>', {class: 'reg-form'}).appendTo($wrapper);
+  var $showHelp = $('<button>', {class: 'show-help', text: 'Show help'});
 
-$(document).ready(function() {
+  function TextInput(label, tooltip) {
+    var inputCount = $form.children().length+1;
+    var $wrapper = $('<div>', {class: 'input-wrapper'}).appendTo($form);
+    var $label = $('<label>', {class: 'form__label', for: 'text-input-'+inputCount, text: label}).appendTo($wrapper);
+    var $input = $('<input>', {class: 'form__input-text', id: 'text-input-'+inputCount}).appendTo($wrapper);
+    var $tooltip = $('<span>', {class: 'tooltip', text: tooltip});
+    var timeoutId, timeoutId2;
 
-$("input").hover( function() {
-        var inputId = $(this).attr("id");
-	
-	console.log($("." + inputId));
-        $("." + inputId).animate( {"opacity": "1"}, 200);
-	
-    }, function() {
-        var inputId = $(this).attr("id");
-        $("." + inputId).animate( {"opacity": "0"}, 100);
+    $input.on({mouseenter: function() {
+      clearTimeout(timeoutId);
+      if (!$input.is( ":focus" ) && $input.val() === ''){
+        $input.showTooltip();
+      }
+    }, focusin: function() {
+      clearTimeout(timeoutId);
+      if (!$input.is(':hover') && $input.val() === '') {
+        $input.showTooltip();
+      }
+    }, mouseleave: function() {
+      if (!$input.is( ":focus" )) {
+        timeoutId = setTimeout($input.hideTooltip, 500);
+      }
+    }, focusout: function() {
+      if (!$input.is(':hover')) {
+        timeoutId = setTimeout($input.hideTooltip, 500);
+      }
+    }, input: function() {
+      if ($input.val() !== '') {
+        if ($wrapper.find('.tooltip')[0]) {
+          console.log('hide');
+          timeoutId = setTimeout($input.hideTooltip, 500);
+        }
+      }
+      else {
+        console.log('show');
+        clearTimeout(timeoutId);
+        $input.showTooltip();
+      }
+    }});
 
-    } );
+    $input.showTooltip = function() {
+      $tooltip.appendTo($wrapper);
+      clearTimeout(timeoutId2);
+      setTimeout(function() {
+        $tooltip.addClass('tooltip--show');
+      }, 10);
+    };
 
-   
-    $("button").click( function() {
-        $(".tip").animate({"opacity": "1"}, "fast");
-    } )
-});
+    $input.hideTooltip = function() {
+      if($('tooltip--show')) {
+        $tooltip.removeClass('tooltip--show');
+        timeoutId2 = setTimeout(function() {
+          $tooltip.remove();
+        }, 500);
+      }
+    };
+
+    return $input;
+  }
+
+
+ var $inputArray = [new TextInput('Firstname', 'Please provide your firstname'),
+                     new TextInput('Lastname', 'Please provide also your lastname'),
+                     new TextInput('Adress', 'Your home or work adress')];
+
+  $showHelp.appendTo($form);
+
+}) ();
