c38fd193b134f113a97582fe98a29c829f2c97ee
AnitaLviv <annet_v@ukr.net>
2016-12-05 12:02:55 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,75 +1,18 @@
-
-
-(function() {
-  var $wrapper = $('<div>', {class: 'wrapper'}).appendTo('body');
-  var $form = $('<form>', {class: 'reg-form'}).appendTo($wrapper);
-  var $showHelp = $('<button>', {class: 'show-help', text: 'Show help'});
-
-  function TextInput(label, tooltip) {
-    var inputCount = $form.children().length+1;
-    var $wrapper = $('<div>', {class: 'input-wrapper'}).appendTo($form);
-    var $label = $('<label>', {class: 'form__label', for: 'text-input-'+inputCount, text: label}).appendTo($wrapper);
-    var $input = $('<input>', {class: 'form__input-text', id: 'text-input-'+inputCount}).appendTo($wrapper);
-    var $tooltip = $('<span>', {class: 'tooltip', text: tooltip});
-    var timeoutId, timeoutId2;
-
-    $input.on({mouseenter: function() {
-      clearTimeout(timeoutId);
-      if (!$input.is( ":focus" ) && $input.val() === ''){
-        $input.showTooltip();
-      }
-    }, focusin: function() {
-      clearTimeout(timeoutId);
-      if (!$input.is(':hover') && $input.val() === '') {
-        $input.showTooltip();
-      }
-    }, mouseleave: function() {
-      if (!$input.is( ":focus" )) {
-        timeoutId = setTimeout($input.hideTooltip, 500);
-      }
-    }, focusout: function() {
-      if (!$input.is(':hover')) {
-        timeoutId = setTimeout($input.hideTooltip, 500);
-      }
-    }, input: function() {
-      if ($input.val() !== '') {
-        if ($wrapper.find('.tooltip')[0]) {
-          console.log('hide');
-          timeoutId = setTimeout($input.hideTooltip, 500);
-        }
-      }
-      else {
-        console.log('show');
-        clearTimeout(timeoutId);
-        $input.showTooltip();
-      }
-    }});
-
-    $input.showTooltip = function() {
-      $tooltip.appendTo($wrapper);
-      clearTimeout(timeoutId2);
-      setTimeout(function() {
-        $tooltip.addClass('tooltip--show');
-      }, 10);
+function createPerson (name,age,job ) {
+    var o = new Object();
+    o.name = name;
+    o.age = age;
+    o.job = job;
+    o.sayName = function(){
+        alert ( this.name );
     };
-
-    $input.hideTooltip = function() {
-      if($('tooltip--show')) {
-        $tooltip.removeClass('tooltip--show');
-        timeoutId2 = setTimeout(function() {
-          $tooltip.remove();
-        }, 500);
-      }
-    };
-
-    return $input;
-  }
+    return o;
+}
 
 
- var $inputArray = [new TextInput('Firstname', 'Please provide your firstname'),
-                     new TextInput('Lastname', 'Please provide also your lastname'),
-                     new TextInput('Adress', 'Your home or work adress')];
+var person1 = createPerson( "Nicholas" , 29 , "Software Eпgiпeer" );
+var person2 = createPerson( "Greg" , 27, "Doctor" );
 
-  $showHelp.appendTo($form);
+console.log(person1);
+console.log(person2);
 
-}) ();
