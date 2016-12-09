59d1516023b5e36792b49d620015c5750ab94740
AnitaLviv <annet_v@ukr.net>
2016-12-09 13:47:35 +0200

    news

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 
-
-$('input').hover(showHint, hideHint);
+$(document).ready(function(){
+$$('input').hover(showHint, hideHint);
   $('button.help, em.help').on('click', showHelp);
   
   function showHint() {
     $(this).parent().find('.hint').remove();
     $(this).parent().append("<span class='hint'>" 
                 + $(this).attr('title') + "</span>");
-  };
+  }
 
   function hideHint() {
     $(this).parent().find('.hint').remove();
-  };
+  }
 
   function showHelp() {
     $('input').each(hideHint);
     $('input').each(showHint);    
-  };
+  }
+});
 
 
